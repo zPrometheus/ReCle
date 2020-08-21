d5dd6f67a8c0ec25478fc2e20a82af57656db5ed
unknown <475398459@qq.com>
2020-08-21 17:54:43 +0800

    update playlist

--- ReClem/src/widgets/StretchHeaderView.cpp
+++ ReClem/src/widgets/StretchHeaderView.cpp
@@ -1,15 +1,41 @@
 #include "StretchHeaderView.h"
 
-
-
+const int StretchHeaderView::kMinimumColumnWidth = 20;
+const int StretchHeaderView::kMagicNumber = 0x502c950f;
 StretchHeaderView::StretchHeaderView(Qt::Orientation orientation, QWidget* parent)
-	:QHeaderView(orientation, parent), mStretchEnabled(false)
+	:QHeaderView(orientation, parent), mStretchEnabled(false),
+	mInMouseMoveEvent(false)
 {
-
-
+	connect(this, SIGNAL(sectionResized(int, int, int)), SLOT(SectionResized(int, int, int)));
+	setMinimumSectionSize(kMinimumColumnWidth);
 
 }
 
+void StretchHeaderView::SectionResized(int logical, int OldSize, int NewSize)
+{
+	if (!mStretchEnabled)
+		return;
+	if (mInMouseMoveEvent) {
+		mColumnWidths[logical] = ColumnWidthType(NewSize) / width();
+
+		int visual = visualIndex(logical);
+		QList<int> LogicalSectionsToResize;
+		for (int i = 0; i < count(); i++)
+		{
+			if (!isSectionHidden(i) && visualIndex(i) > visual)
+				LogicalSectionsToResize << i;
+		}
+		//......to be continued ,
+
+
+	}
+
+}
+
+void StretchHeaderView::setModel(QAbstractItemModel* model)
+{
+	QHeaderView::setModel(model);
+}
 
 StretchHeaderView::~StretchHeaderView()
 {
--- ReClem/src/widgets/StretchHeaderView.h
+++ ReClem/src/widgets/StretchHeaderView.h
@@ -8,10 +8,23 @@ public:
 	StretchHeaderView(Qt::Orientation orientation,QWidget* parent=nullptr);
 	~StretchHeaderView();
 
+	typedef double ColumnWidthType;
+	static const int kMinimumColumnWidth;
+	static const int kMagicNumber;
+
+	void setModel(QAbstractItemModel* model);
+
 	bool isStretchEnabled() const { return mStretchEnabled; };
 private:
 	bool mStretchEnabled;
+	bool mInMouseMoveEvent;
+	QVector<ColumnWidthType> mColumnWidths;
 
+
+
+	private slots:
+	void SectionResized(int logical, int OldSize, int NewSize);
+	
 
 };
 
